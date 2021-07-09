86227eb2e2926507bf7f0c9aa114209a9b872a9f
Amanda Domingos <ardlima4@gmail.com>
2021-07-09 12:05:18 -0300

    first commit

--- R/hello.R
+++ R/hello.R
@@ -1,19 +1,35 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' @title tier_protocol
+#'
+#' @description Provides documentation for your R project,
+#' through version 4.0 of the TIER protocol, developed by project TIER.
+#'
+#' @param tier Defaults to TRUE.
+#' 
+#' @return when TRUE, returns the 4.0 version of TIER protocol. 
+#' @return When false, returns a minimal version of TIER protocol.  
+#'
+#' @example library(TIER)
+#' tier_protocol(tier = TRUE)}
+#'
+#' @export
 
-hello <- function() {
-  print("Hello, mundo!")
+tier_protocol <- function(tier=TRUE){
+  if(tier==TRUE){  dir.create("MyProject")
+    dir.create("MyProject/Data")
+    dir.create("MyProject/Data/IntermediateData")  
+    dir.create("MyProject/Data/AnalysisData")
+    dir.create("MyProject/Data/OriginalData")
+    dir.create("MyProject/Output")
+    dir.create("MyProject/Scripts")
+    dir.create("MyProject/Scripts/Analysis")
+    dir.create("MyProject/Scripts/DataAppendix")
+    dir.create("MyProject/Scripts/Processing")}
+  else {
+    dir.create("MyProject")
+    dir.create("MyProject/Data")
+    dir.create("MyProject/Data/AnalysisData")
+    dir.create("MyProject/Data/OriginalData")
+    dir.create("MyProject/Output")
+    dir.create("MyProject/Scripts")
+  }
 }
-
